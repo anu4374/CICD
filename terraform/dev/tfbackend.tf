f5c60746c8b2cba174f55393f6e8110b0513f7f2
AnuhyaDasari <35353117+anu4374@users.noreply.github.com>
2019-03-13 12:15:16 -0700

    Update tfbackend.tf

--- terraform/dev/tfbackend.tf
+++ terraform/dev/tfbackend.tf
@@ -1,9 +1,4 @@
 terraform {
-  backend "s3" {
-    bucket  = "cicd-anuhya"
-    key     = "tf/us-east-2/dev.json"
-    region  = "us-east-2"
-    shared_credentials_file = "/home/ec2-user/.aws/credentials"
-    profile = "default"
+  backend "local" {
+    path = "/var/lib/jenkins/anuhya/terraform.tfstate"
   }
-}
